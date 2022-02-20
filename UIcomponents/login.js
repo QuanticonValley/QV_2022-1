cbbe399f80c6f4e837cf5ab82938b0fd3ede0bac
Baorozcoc <36195164+Baorozcoc@users.noreply.github.com>
2022-02-19 21:30:20 -0500

    Cambio 3_6
    c3_6

--- UIcomponents/login.js
+++ UIcomponents/login.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import Link from 'next/link'
+import LoginForm from './loginform'
 
 const Panel = styled.div`
     border: 2px solid #fff;
@@ -20,60 +20,9 @@ const Button= styled.button`
     border: 2px solid #172BEF;
     border-radius: .5em;
 `
-const Button2= styled.input`
-    width: 150px;
-    color: #172BEF;
-    font-weight: bold;
-    padding: .5em 1em;
-    margin: 1em auto;
-    border: 2px solid #172BEF;
-    border-radius: .5em;
-`
-const Registro=styled.div`
-    text-align: center;
-    display:flex;
-    flex-direction:row;
-`
-const Input=styled.input`
-    border-radius: .5em;
-    border: 1px solid #fff;
-    height: 100%;
-    background-color: #bbbbbb;
-`
-
-function RegistroLogin(login){
-    let code;
-    console.log(login);
-    if (login==true) {
-        code=(
-        <Registro>
-            <h2>INICIA SESIÓN</h2>
-            <form>
-                <Input type="text" placeholder='Tu Correo'></Input>
-                <Input type="text" placeholder='Tu Contraseña'></Input>
-                <Link href="/principal">
-                    <Button2 type="submit" value="¡AQUI VAMOS!"></Button2>
-                </Link>
-            </form>
-        </Registro>)
-    } else {
-        code=(
-        <Registro>
-            <h2>REGÍSTRATE</h2>
-            <form>
-                <Input type="text" placeholder='Tu Correo'></Input>
-                <Input type="text" placeholder='Tu Contraseña'></Input>
-                <Input type="text" placeholder='Confirma Contraseña'></Input>
-                <Link href="/principal">
-                    <Button2 type="submit" value="¡AQUI VAMOS!"></Button2>
-                </Link>
-            </form>
-        </Registro>)
-    }
-    return code;
-}
 
 function cambio(decision){
+    console.log(login);
     decision===1?login=true:login=false;
 }
 
@@ -87,7 +36,9 @@ const Login = () => {
                 <Button onClick={()=>cambio(2)}>Registrarse</Button>
             </Encabezado>
             
-            {RegistroLogin(login)}
+            <LoginForm
+            login={login}
+            ></LoginForm>
 
 
         </Panel>
---
+++ UIcomponents/loginform.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+import Link from 'next/link'
+
+const Button2= styled.input`
+    width: 150px;
+    color: #172BEF;
+    font-weight: bold;
+    padding: .5em 1em;
+    margin: 1em auto;
+    border: 2px solid #172BEF;
+    border-radius: .5em;
+`
+const Registro=styled.div`
+    text-align: center;
+    display:flex;
+    flex-direction:row;
+`
+const Input=styled.input`
+    border-radius: .5em;
+    border: 1px solid #fff;
+    height: 100%;
+    background-color: #bbbbbb;
+`
+
+const LoginForm = (login) => {
+    return (
+        <div>
+        {login?
+            <Registro>
+            <h2>INICIA SESIÓN</h2>
+            <form>
+                <Input type="text" placeholder='Tu Correo'></Input>
+                <Input type="text" placeholder='Tu Contraseña'></Input>
+                <Link href="/principal">
+                    <Button2 type="submit" value="¡AQUI VAMOS!"></Button2>
+                </Link>
+            </form>
+        </Registro>
+        :
+        <Registro>
+            <h2>REGÍSTRATE</h2>
+            <form>
+                <Input type="text" placeholder='Tu Correo'></Input>
+                <Input type="text" placeholder='Tu Contraseña'></Input>
+                <Input type="text" placeholder='Confirma Contraseña'></Input>
+                <Link href="/principal">
+                    <Button2 type="submit" value="¡AQUI VAMOS!"></Button2>
+                </Link>
+            </form>
+        </Registro>
+
+        }
+        </div> 
+     );
+}
+ 
+export default LoginForm;
