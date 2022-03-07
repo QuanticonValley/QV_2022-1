1f39c08a83d5b4c332312663afb268e0d4807b3b
Baorozcoc <36195164+Baorozcoc@users.noreply.github.com>
2022-03-07 01:51:31 +0000

    Deploying to gh-pages from @ TPI-Desarrollo/QV_2022-1@2341020e7a1245bee33f8471c87ecb979cf0c9c1 🚀

---
+++ _next/static/chunks/pages/profile/components/mainComp-0491726de1d1c173.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[100],{4812:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/profile/components/mainComp",function(){return o(4264)}])}},function(n){n.O(0,[577,735,638,513,774,888,179],(function(){return _=4812,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
