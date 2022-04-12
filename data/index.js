505fed107c0e7a51941f0a74e874cd924661123d
Baorozcoc <36195164+Baorozcoc@users.noreply.github.com>
2022-04-12 00:45:23 +0000

    Deploying to gh-pages from @ TPI-Desarrollo/QV_2022-1@18edc339cc346cfe97cdb3ddae3065e2798cfff8 🚀

--- data/index.js
+++ data/index.js
@@ -1,8 +1,6 @@
 import etesc from './etesc.json'
-import zones from './zones.json'
 import lobbydata from './zonesCont/lobby.json'
 import asesordata from './zonesCont/asesor.json'
-import webinardata from './zonesCont/webinar.json'
 import noti from './noti.json'
 import info from './info.json'
 import office from './office.json'
@@ -15,8 +13,7 @@ export const getstate = () => state
 export const statePiso = () => state.piso
 //statePisoActual
 export const statePisoA = () => state.pisoMax
-//stateUpdates
-export const stateUpdates = () => state.lastUpdates
+
 //Principal
 export const getLogosExt = () => logos.logos
 //Progress
@@ -27,16 +24,9 @@ export const getEtesc = (grp, piso) => etesc[grp][piso]
 export const getPisoTitle = (piso) => etesc["pisos"][piso]
 export const getPisoData = (grp,piso) =>etesc[grp][piso]
 
-// ZONES
-export const getZones = () => zones
-
-// CONTENT
-export const getLobbyDataByGroup = (grp) => lobbydata[grp]
-
 //ACTORS
 export const getMentores=()=>actors.mentores
-// LOBBY
-export const getLobbyData = (grp, piso) => lobbydata[grp][piso]
+// HALLFAME
 export const getHFData = () => lobbydata.hallFame
 
 // ASESOR
@@ -44,8 +34,6 @@ export const getAsesorSrc = (grp, piso) => asesordata.src[grp][piso]
 export const getAsesorVir = () => asesordata.virtual
 export const getAsesorFaq = () => asesordata.faqs
 
-// WEBINARS
-export const getWebinarData = () => webinardata
 
 // NOTI
 export const getNoti = () => noti
