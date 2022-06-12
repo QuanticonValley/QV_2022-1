9d6ce27359d81da54d6d549b8a4451026f07a07d
pvelosa <62478977+pvelosa@users.noreply.github.com>
2022-06-12 01:53:56 +0000

    Deploying to gh-pages from @ TPI-Desarrollo/QV_2022-1@00be027702d307058b9efd74c64ae59e8a0bcc2c 🚀

---
+++ _next/static/chunks/pages/profile/components/mainComp-e8c74e7d52c2d6c4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[100],{96:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/profile/components/mainComp",function(){return o(8036)}])}},function(n){n.O(0,[577,735,89,370,774,888,179],(function(){return _=96,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
