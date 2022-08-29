9ebaac3ffa833fbe8da708c77c1737c34922b87f
Miguel Medellín <mamedellinr@unal.edu.co>
2022-08-29 09:20:13 -0500

    add stats

--- pages/roleTest/[type].js
+++ pages/roleTest/[type].js
@@ -26,7 +26,7 @@ const SelItem = styled.div`
 	margin: .5em 1em;
 	font-weight: bold;
 	font-size: 1em;
-	color: ${p=>p.active ?'black':'#999'};
+	color: ${p => p.active ? 'black' : '#999'};
 	:hover {
 		cursor: pointer;
 	}
@@ -39,52 +39,48 @@ const SelBar = styled.div`
 	position: absolute;
 	background: #172BEF;
 	height: 3px;
-	width: ${p=>p.w || '60'}px;
-	top: ${p=>p.y || '0'}px;
-	left: ${p=>p.x || '0'}px;
+	width: ${p => p.w || '60'}px;
+	top: ${p => p.y || '0'}px;
+	left: ${p => p.x || '0'}px;
 	transition: .3s ease;
 `
 
 const RoleTest = () => {
-  const router = useRouter()
-  const type = router.query.type
+   const router = useRouter()
+   const type = router.query.type
 
-	const selRef = useRef()
-	const barRef = useRef()
-	const [selPos, setSelPos] = useState()
-	const [barPos, setBarPos] = useState()
-	const bx = barPos?.x - selPos?.x
-	const by = barPos?.y - selPos?.y + barPos?.height + 2
-	const bw = barPos?.width
+   const selRef = useRef()
+   const barRef = useRef()
+   const [selPos, setSelPos] = useState()
+   const [barPos, setBarPos] = useState()
+   const bx = barPos?.x - selPos?.x
+   const by = barPos?.y - selPos?.y + barPos?.height + 2
+   const bw = barPos?.width
 
-	useEffect(()=>{
-		setSelPos(selRef?.current?.getBoundingClientRect())
-		setBarPos(barRef?.current?.getBoundingClientRect())
-	}, [type])
+   useEffect(() => {
+      setSelPos(selRef?.current?.getBoundingClientRect())
+      setBarPos(barRef?.current?.getBoundingClientRect())
+   }, [type])
 
-	return <QV pg="Test de roles">
-		<Selector ref={selRef}>
-			<Link href='/roleTest/stats' passHref>
-				<SelItem 
-					active={type==='stats'}
-					ref={type==='stats'? barRef : null}
-				>
-				ESTADÍSTICAS
-			</SelItem></Link>
-			<Link href='/roleTest/cards' passHref>
-				<SelItem 
-					active={type==='cards'}
-					ref={type==='cards'? barRef : null}
-				>
-				JUEGO DE CARTAS
-			</SelItem></Link>
-			<SelBar x={bx} y={by} w={bw}/>
-		</Selector>
-		<Cont>
-			{type === 'stats'&& (<Stats />)}
-			{type === 'cards'&& (<Role />)}
-		</Cont>
-	</QV>
+   return <QV pg="Test de roles">
+      <Selector ref={selRef}>
+         <Link href='/roleTest/stats' passHref>
+            <SelItem active={type === 'stats'} ref={type === 'stats' ? barRef : null}>
+               ESTADÍSTICAS
+            </SelItem>
+         </Link>
+         <Link href='/roleTest/cards' passHref>
+            <SelItem active={type === 'cards'} ref={type === 'cards' ? barRef : null}>
+               JUEGO DE CARTAS
+            </SelItem>
+         </Link>
+         <SelBar x={bx} y={by} w={bw} />
+      </Selector>
+      <Cont>
+         {type === 'stats' && (<Stats />)}
+         {type === 'cards' && (<Role />)}
+      </Cont>
+   </QV>
 }
 
 export default RoleTest;
--- pages/roleTest/pages/stats.js
+++ pages/roleTest/pages/stats.js
@@ -1,4 +1,3 @@
-import styled from 'styled-components';
 import AliceCarousel from 'react-alice-carousel';
 import { prefix } from '../../../utils/prefix';
 import { useMainState } from '../../../libs/stateHooks'
@@ -11,30 +10,22 @@ const Stats = () => {
    if (mState.group == "dos") {
       return (
          <div>
-            <br />
-            <AliceCarousel disableButtonsControls>
-               <img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Emprendimiento, Intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
-               <img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Emprendimiento, Tabla, Intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
-               <img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Emprendimiento, NO Intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
-               <img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Emprendimiento, Tabla, NO Intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
-            </AliceCarousel>
+            <br /><img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Emprendimiento, Intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
+            <br /><img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Emprendimiento, Tabla, Intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
+            <br /><img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Emprendimiento, NO Intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
+            <br /><img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Emprendimiento, Tabla, NO Intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
          </div>
-
       );
    }
 
    else {
       return (
          <div>
-            <br />
-            <AliceCarousel disableButtonsControls>
-               <img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Innovación Abierta, Intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
-               <img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Innovación Abierta, Tabla, Intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
-               <img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Innovación Abierta, NO intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
-               <img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Innovación Abierta, Tabla, NO Intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
-            </AliceCarousel>
+            <br /><img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Innovación Abierta, Intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
+            <br /><img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Innovación Abierta, Tabla, Intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
+            <br /><img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Innovación Abierta, NO intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
+            <br /><img width="90%" src={`${prefix}/imgs/cards/stats/Roles de Trabajo, Innovación Abierta, Tabla, NO Intereses.png`} onDragStart={handleDragStart} className="sliderimg" />
          </div>
-
       );
    }
 }
