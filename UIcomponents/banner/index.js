893904b00a06ebb8461e7c4dfd65c7b60c888736
Miguel Medellín <mamedellinr@unal.edu.co>
2022-10-12 13:53:56 -0500

    update banner

--- UIcomponents/banner/index.js
+++ UIcomponents/banner/index.js
@@ -12,17 +12,24 @@ const Gallery = () => {
    if (mState.group == "dos") {
       return (
          <AliceCarousel autoPlay disableButtonsControls autoPlayInterval="6000" >
-            <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/01.jpg`} onDragStart={handleDragStart} className="sliderimg" />
-            <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/02.jpg`} onDragStart={handleDragStart} className="sliderimg" />
-            <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/03.jpg`} onDragStart={handleDragStart} className="sliderimg" />
-            <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/04.jpg`} onDragStart={handleDragStart} className="sliderimg" />
+            <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/01.png`} onDragStart={handleDragStart} className="sliderimg" />
 
-            <a href="https://www.eltiempo.com/tecnosfera/novedades-tecnologia/diggi-pymes-asi-es-el-erp-de-impacto-social-703700" rel="noreferrer" target="_blank">
-               <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/05.jpg`} onDragStart={handleDragStart} className="sliderimg" />
+            <a href="https://micampus.unal.edu.co/mod/page/view.php?id=856287" rel="noreferrer" target="_blank">
+               <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/02.png`} onDragStart={handleDragStart} className="sliderimg" />
+            </a>
+
+            <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/03.png`} onDragStart={handleDragStart} className="sliderimg" />
+            <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/04.png`} onDragStart={handleDragStart} className="sliderimg" />
+            <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/05.png`} onDragStart={handleDragStart} className="sliderimg" />
+            <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/06.png`} onDragStart={handleDragStart} className="sliderimg" />
+            <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/07.png`} onDragStart={handleDragStart} className="sliderimg" />
+
+            {/* <a href="https://www.eltiempo.com/tecnosfera/novedades-tecnologia/diggi-pymes-asi-es-el-erp-de-impacto-social-703700" rel="noreferrer" target="_blank">
+               <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/05.png`} onDragStart={handleDragStart} className="sliderimg" />
             </a>
             <a href="https://twitter.com/UNALOficial/status/1572619231685971968?t=m-K1B3_jz4nfJvH7YGjYNQ&s=08" rel="noreferrer" target="_blank">
                <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/06.png`} onDragStart={handleDragStart} className="sliderimg" />
-            </a>
+            </a> */}
          </AliceCarousel>
       );
    }
@@ -30,17 +37,24 @@ const Gallery = () => {
    else {
       return (
          <AliceCarousel autoPlay disableButtonsControls autoPlayInterval="6000">
-            <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/01.jpg`} onDragStart={handleDragStart} className="sliderimg" />
-            <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/02.jpg`} onDragStart={handleDragStart} className="sliderimg" />
-            <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/03.jpg`} onDragStart={handleDragStart} className="sliderimg" />
-            <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/04.jpg`} onDragStart={handleDragStart} className="sliderimg" />
+            <img width="90%" src={`${prefix}/imgs/banner/innovacion/01.png`} onDragStart={handleDragStart} className="sliderimg" />
 
-            <a href="https://www.eltiempo.com/tecnosfera/novedades-tecnologia/diggi-pymes-asi-es-el-erp-de-impacto-social-703700" rel="noreferrer" target="_blank">
+            <a href="https://micampus.unal.edu.co/mod/page/view.php?id=856282" rel="noreferrer" target="_blank">
+               <img width="90%" src={`${prefix}/imgs/banner/innovacion/02.png`} onDragStart={handleDragStart} className="sliderimg" />
+            </a>
+
+            <img width="90%" src={`${prefix}/imgs/banner/innovacion/03.png`} onDragStart={handleDragStart} className="sliderimg" />
+            <img width="90%" src={`${prefix}/imgs/banner/innovacion/04.png`} onDragStart={handleDragStart} className="sliderimg" />
+            <img width="90%" src={`${prefix}/imgs/banner/innovacion/05.png`} onDragStart={handleDragStart} className="sliderimg" />
+            <img width="90%" src={`${prefix}/imgs/banner/innovacion/06.png`} onDragStart={handleDragStart} className="sliderimg" />
+            <img width="90%" src={`${prefix}/imgs/banner/innovacion/07.png`} onDragStart={handleDragStart} className="sliderimg" />
+
+            {/* <a href="https://www.eltiempo.com/tecnosfera/novedades-tecnologia/diggi-pymes-asi-es-el-erp-de-impacto-social-703700" rel="noreferrer" target="_blank">
                <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/05.jpg`} onDragStart={handleDragStart} className="sliderimg" />
             </a>
             <a href="https://twitter.com/UNALOficial/status/1572619231685971968?t=m-K1B3_jz4nfJvH7YGjYNQ&s=08" rel="noreferrer" target="_blank">
                <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/06.png`} onDragStart={handleDragStart} className="sliderimg" />
-            </a>
+            </a> */}
          </AliceCarousel>
       );
    }
