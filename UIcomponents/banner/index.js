181d157f0c9ab914f3c01a148c3b63561e95c089
Miguel Medellín <mamedellinr@unal.edu.co>
2022-10-12 14:17:45 -0500

    update banner

--- UIcomponents/banner/index.js
+++ UIcomponents/banner/index.js
@@ -24,12 +24,9 @@ const Gallery = () => {
             <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/06.png`} onDragStart={handleDragStart} className="sliderimg" />
             <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/07.png`} onDragStart={handleDragStart} className="sliderimg" />
 
-            {/* <a href="https://www.eltiempo.com/tecnosfera/novedades-tecnologia/diggi-pymes-asi-es-el-erp-de-impacto-social-703700" rel="noreferrer" target="_blank">
-               <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/05.png`} onDragStart={handleDragStart} className="sliderimg" />
-            </a>
             <a href="https://twitter.com/UNALOficial/status/1572619231685971968?t=m-K1B3_jz4nfJvH7YGjYNQ&s=08" rel="noreferrer" target="_blank">
-               <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/06.png`} onDragStart={handleDragStart} className="sliderimg" />
-            </a> */}
+               <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/_06.png`} onDragStart={handleDragStart} className="sliderimg" />
+            </a>
          </AliceCarousel>
       );
    }
@@ -49,12 +46,9 @@ const Gallery = () => {
             <img width="90%" src={`${prefix}/imgs/banner/innovacion/06.png`} onDragStart={handleDragStart} className="sliderimg" />
             <img width="90%" src={`${prefix}/imgs/banner/innovacion/07.png`} onDragStart={handleDragStart} className="sliderimg" />
 
-            {/* <a href="https://www.eltiempo.com/tecnosfera/novedades-tecnologia/diggi-pymes-asi-es-el-erp-de-impacto-social-703700" rel="noreferrer" target="_blank">
-               <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/05.jpg`} onDragStart={handleDragStart} className="sliderimg" />
-            </a>
             <a href="https://twitter.com/UNALOficial/status/1572619231685971968?t=m-K1B3_jz4nfJvH7YGjYNQ&s=08" rel="noreferrer" target="_blank">
-               <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/06.png`} onDragStart={handleDragStart} className="sliderimg" />
-            </a> */}
+               <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/_06.png`} onDragStart={handleDragStart} className="sliderimg" />
+            </a>
          </AliceCarousel>
       );
    }
