f1b2344377cd10629c586b9579b2f7363ba1960e
Miguel Medellín <mamedellinr@unal.edu.co>
2022-10-18 21:23:29 -0500

    update links

--- UIcomponents/banner/index.js
+++ UIcomponents/banner/index.js
@@ -13,7 +13,6 @@ const Gallery = () => {
       return (
          <AliceCarousel autoPlay disableButtonsControls autoPlayInterval="6000" >
             <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/01.png`} onDragStart={handleDragStart} className="sliderimg" />
-
             <a href="https://youtu.be/wri7qUF8_TE" rel="noreferrer" target="_blank">
                <img width="90%" src={`${prefix}/imgs/banner/emprendimiento/02.png`} onDragStart={handleDragStart} className="sliderimg" />
             </a>
@@ -35,7 +34,6 @@ const Gallery = () => {
       return (
          <AliceCarousel autoPlay disableButtonsControls autoPlayInterval="6000">
             <img width="90%" src={`${prefix}/imgs/banner/innovacion/01.png`} onDragStart={handleDragStart} className="sliderimg" />
-
             <a href="https://youtu.be/wri7qUF8_TE" rel="noreferrer" target="_blank">
                <img width="90%" src={`${prefix}/imgs/banner/innovacion/02.png`} onDragStart={handleDragStart} className="sliderimg" />
             </a>
