9236adacff2bcaf218fe773f04d5058854a6d40b
Chranium <mamedellinr@unal.edu.co>
2023-01-26 10:15:15 -0500

    install packages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
-}
+   reactStrictMode: true,
+   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+   webpack: (config, options) => {
+      config.module.rules.push({
+         test: /\.html$/i,
+         loader: "html-loader",
+         options: {
+            sources: false,
+         },
+      });
+
+      return config;
+   }
+};
